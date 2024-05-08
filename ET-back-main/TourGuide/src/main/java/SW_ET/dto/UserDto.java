156025d2ddda28819ad82267e817d2c9f990bb5b
Federico-15 <peter1256@skuniv.ac.kr>
2024-05-08 19:09:03 +0900

    회원가입 DB 연동

--- ET-back-main/TourGuide/src/main/java/SW_ET/dto/UserDto.java
+++ ET-back-main/TourGuide/src/main/java/SW_ET/dto/UserDto.java
@@ -1,12 +1,31 @@
 package SW_ET.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class UserDto {
     private String userName;
     private String userEmail;
     private String userPassword;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
 }
